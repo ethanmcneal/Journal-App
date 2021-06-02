a61338b3bc9b5b1dc3d330c1f54667b124a36b14
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-02 15:04:39 -0600

    api test... working?

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,27 +1,27 @@
 import { Text, View } from "react-native";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function Home() {
-    const [test, setTest] = useState(null)
-    useEffect(() => {
-        getTest()
-    })
+function Home() {
+  const [test, setTest] = useState(null);
+  useEffect(() => {
+    getTest();
+  }, []);
 
-    const getTest = async() => {
-        try {
-            debugger
-            let res = await axios.get('/api/api_test_2')
-            setTest(res)
-            console.log(res)
-        } catch (error) {
-            console.log(error)
-        }
+  const getTest = async () => {
+    try {
+      let res = await axios.get("http://localhost:3001/api/api_test_2");
+      setTest(res.data);
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <View>
-  <Text>Home</Text>
-  <Text>{JSON.stringify(test)}</Text>
-  </View>
-    )
+  };
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>{JSON.stringify(test)}</Text>
+    </View>
+  );
 }
+
+export default Home;
