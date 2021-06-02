65dfae2948d5a33212aec629d36607d9e6362725
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-02 12:59:06 -0600

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    get '/api_test', to: 'static#api_test'
+    get '/api_test_2', to: 'static#api_test_2'
+  end
+end
